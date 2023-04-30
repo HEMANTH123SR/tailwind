06207869c2e8b015feb328dc0268867715e6a1c2
hemanth <applicationcomputer257@gmail.com>
2023-04-30 20:55:07 +0530

    slider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Nav from './components/Nav.js';
 import Pop from "./components/Pop.js";
 import BestSection from './components/BestSection';
+import Slider from './components/Slider';
 function App() {
   return (
     <div className="font-sans font-medium">
   <Nav/>
   <Pop/>
   <BestSection/>
+  <Slider/>
     </div>
   );
 }
---
+++ src/components/Slider.js
@@ -0,0 +1,44 @@
+import summerBeverage from "../assets/Group_1701_d6192f3622.png"
+
+const Slider=()=>{
+  
+    return(
+        <div className="m-32 bg-pink-300 h-56
+        rounded-lg
+        flex flex-row
+        space-x-20
+        ">
+   <img src={summerBeverage} />
+   <div className="space-y-10">
+
+    <div className="space-y-2 ">
+        <h1 className="pt-4"></h1>
+        <h1></h1>
+    <h1>Refresh & Rejuvenate With</h1>
+    <h1 className="text-2xl">Summer Beverage</h1>
+    <h1>indulge in the exquisite range of Summer Beverages to beat the heat.</h1>
+    </div>
+   
+   <div>
+    <h1>Starting from </h1>
+    <h1>₹499</h1>
+   </div>
+   </div>
+
+   <div >
+    <button
+    className="mt-40 
+    bg-green-600
+    px-14
+    py-3
+    rounded-full
+    text-slate-50
+
+    "
+    >Order Now</button>
+   </div>
+        </div>
+    )
+}
+
+export default Slider;
