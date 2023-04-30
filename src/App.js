976f334e42f92c22b735b04da78d6e69cabf975e
hemanth <applicationcomputer257@gmail.com>
2023-04-30 19:35:58 +0530

    best section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Nav from './components/Nav.js';
-import Pop from "./components/Pop.js"
+import Pop from "./components/Pop.js";
+import BestSection from './components/BestSection';
 function App() {
   return (
     <div className="font-sans font-medium">
   <Nav/>
   <Pop/>
+  <BestSection/>
     </div>
   );
 }
--- src/components/BestSection.js
+++ src/components/BestSection.js
@@ -1,7 +1,58 @@
+import Coffee from "../assets/Bestseller.jpg"
+import CoffeeAtHome from "../assets/CoffeeAtHome.jpg"
+import Drinks from "../assets/Drinks.jpg"
+import Food from "../assets/Food.jpg"
+import Merch from "../assets/Merchandise.jpg"
+import choclate from "../assets/ReadyToEat.jpg"
 const BestSection=()=>{
     return(
+        <div className="h-44 
+
+        mx-28
+        ">
+    <h1 className="
+    py-8
+    font-bold
+    text-slate-900
+
+    
+    text-2xl
+    "
+    >Handcrafted Curations</h1>
+    <div className="flex flex-row space-x-24">
         <div>
-            
+        <img src={Coffee}  className="w-28 h-28 rounded-full" />
+        <h1 className="text-center">Bestseller</h1>
+        </div>
+
+        <div>
+        <img src={Drinks} className="w-28 h-28 rounded-full" />
+        <h1 className="text-center">Drinks</h1>
+        </div>
+
+        <div>
+        <img src={Food}  className="w-28 h-28 rounded-full" />
+        <h1 className="text-center">Food</h1>
+        </div>
+
+        <div>
+        <img src={Merch}  className="w-28 h-28 rounded-full" />
+        <h1 className="text-center">Merchandise</h1>
+        </div>
+
+        <div>
+        <img src={choclate} className="w-28 h-28 rounded-full" />
+        <h1 className="text-center">Ready to Eat</h1>
+        </div>
+       
+        <div>
+        <img src={CoffeeAtHome} className="w-28 h-28 rounded-full"  />
+     
+        <h1 className="text-center"> Home</h1>
+        
+       
+        </div>
+    </div>
         </div>
     )
 }
